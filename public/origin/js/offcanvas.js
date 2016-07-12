32319830ec22ca7dbb1cd46f32150e0f0285a178
chukui <mark@MarkdeMacBook-Pro.local>
2016-07-12 17:46:58 +0800

    no message

--- public/origin/js/offcanvas.js
+++ public/origin/js/offcanvas.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
+  $('.nav-cate').hover(
+  	function(){
+  		$('.dropdown-menu').show();
+  	},
+  	function(){
+  		$('.dropdown-menu').hide();
+  	}
+  );
 });
