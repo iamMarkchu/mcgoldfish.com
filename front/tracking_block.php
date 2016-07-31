00b6b286beaaec7571e7110884b49e5b2613e520
chukui <mark@MarkdeMBP.lan>
2016-07-31 08:40:34 +0800

    no message

--- front/tracking_block.php
+++ front/tracking_block.php
@@ -1,6 +1,6 @@
 <?php
-defined('IN_DS') or die('Hacking attempt');
-include_once 'v4/incoming.php';
-include_once 'v4/pv.php';
-filter_source_tag();
+// defined('IN_DS') or die('Hacking attempt');
+// include_once 'v4/incoming.php';
+// include_once 'v4/pv.php';
+// filter_source_tag();
 
